850ad685655f67a66e81c43ce7a3c3ed97c9b7e0
shann606 <shann606@gmail.com>
2025-09-19 15:33:24 +0530

    first commit

---
+++ src/test/java/com/ecom/customerservice/CustomerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
