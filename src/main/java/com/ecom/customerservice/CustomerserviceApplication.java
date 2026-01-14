63955547c4f473bd95110867b446fcf44230d782
shann606 <shann606@gmail.com>
2026-01-14 16:18:42 +0530

    Enabled caching using Ehcaching

--- src/main/java/com/ecom/customerservice/CustomerserviceApplication.java
+++ src/main/java/com/ecom/customerservice/CustomerserviceApplication.java
@@ -2,6 +2,7 @@ package com.ecom.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.ecom.customerservice.repository")
 @EnableFeignClients
+@EnableCaching
 public class CustomerserviceApplication {
 
 	public static void main(String[] args) {
